3073707d7c75508c760230f1f6c83c9bda2939c9
FromSaffronCity <yasar199510@gmail.com>
2026-02-05 14:27:29 -0500

    Tables and seeds generation code added and modified

--- src/pumpkinhash_tables_generator.cpp
+++ src/pumpkinhash_tables_generator.cpp
@@ -4,16 +4,16 @@ int main(int argc, char **argv)
 {
     if (argc != 4)
     {
-        cerr << "Invalid number of arguments provided!\nCorrect usage: ./pumpkinhash_tables_generator [windowSizeN] [paramD] [numTablesFileVersions]" << endl;
+        cerr << "Invalid number of command-line arguments provided!\nCorrect usage: ./pumpkinhash_tables_generator [windowSizeN] [paramD] [numTablesFileVersions]" << endl;
 
         return 1;
     }
 
     int windowSizeN = stoi(argv[1]), paramD = stoi(argv[2]), numTablesFileVersions = stoi(argv[3]);
 
-    map<char, int> alphabet = {{'A', 0}, {'C', 1}, {'G', 2}, {'T', 3}};
+    map<char, int> defaultAlphabet = {{'A', 0}, {'C', 1}, {'G', 2}, {'T', 3}};
 
-    PumpkinHash pumpkinHash(windowSizeN, paramD, alphabet);
+    PumpkinHash pumpkinHash(windowSizeN, paramD, defaultAlphabet);
 
     cout << "PumpkinHash object created with N = " << windowSizeN << ", D = " << paramD << ", and default alphabet..." << endl;
 
